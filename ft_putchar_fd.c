c97b3365897b95f7788083a13277e80e1a2dae35
Leonardo Marino <leo-marino@hotmail.it>
2024-12-04 19:03:56 +0100

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -11,4 +11,6 @@ void	ft_putchar_fd(char c, int fd)
 0	Represents standard input.
 1	Represents standard output.
 2	Represents standard error.
+3~9	Writes whitin a specific file.
+10 >>	writes whitin several files.
 */
