a2831b8af9f5212b0d93ab34b9b0776364930fb6
Leonardo Marino <leo-marino@hotmail.it>
2024-12-23 12:27:02 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,5 +1,6 @@
 //The  memchr()  function scans the initial n bytes of the memory area pointed to by s for the first instance of
 //       c.  Both c and the bytes of the memory area pointed to by s are interpreted as unsigned char.
+//	 Returns a pointer to the matching byte or NULL if the character does not occur in the given memory area.
 
 #include "libft.h"
 
