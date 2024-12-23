2fe1c88a61a353c0a15482372ec70897b36ae031
Leonardo Marino <leo-marino@hotmail.it>
2024-12-23 12:42:32 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,3 +1,9 @@
+// The strlcat() function appends the NUL-terminated string src to the end of dst. It will append at most size - strlen(dst) - 1 bytes, NUL-terminating the result.
+// Returns the total length of the string they tried to create. For strlcat() that means the initial length of dst plus the length of src.
+// 	Note, however, that if strlcat() traverses size characters without finding a NUL,
+// 	the length of the string is considered to be size and the destination string will not be NULL-terminated.
+
+	
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
