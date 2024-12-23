bf934e41126c1f991576113601fd50c21cec2ca6
Leonardo Marino <leo-marino@hotmail.it>
2024-12-23 12:53:07 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,3 +1,8 @@
+// Locates the first occurrence of the  null-terminated  string little in the string big, where not more than len characters are searched.
+// Characters that appear after a '\0' character  are not  searched.
+// If little is an empty string, big is returned; if little occurs nowhere in big, NULL is returned;
+// 	otherwise a pointer to	the first character of the first occurrence of little is returned.
+
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
