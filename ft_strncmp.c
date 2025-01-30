568ff081f621e3101f9e15b512431ad51d72c06a
Leonardo Marino <leo-marino@hotmail.it>
2025-01-30 18:22:35 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,8 +1,6 @@
+#include "libft.h"
 //Compares the first (at most) n bytes of the two strings s1 and s2. It returns an integer less than, equal to, or greater than zero if  s1  is
 //     found, respectively, to be less than, to match, or be greater than s2.
-
-#include "libft.h"
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
