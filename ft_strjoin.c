4174037ae1132fc508d51868800652ef91c6ba21
Leonardo Marino <leo-marino@hotmail.it>
2025-01-30 18:21:31 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,7 +1,5 @@
-//Allocates (with malloc()) and returns a new string, which is the result of the concatenation of ’s1’ and ’s2’.
-
 #include "libft.h"
-
+//Allocates (with malloc()) and returns a new string, which is the result of the concatenation of ’s1’ and ’s2’.
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t			i;
