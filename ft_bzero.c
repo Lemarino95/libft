6f1b6acc40a6a19a8a294b8c182f083550781eb6
Leonardo Marino <leo-marino@hotmail.it>
2025-01-30 18:15:01 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,8 +1,6 @@
+#include "libft.h"
 //The bzero() function erases the data in the n bytes of the memory starting at the location pointed to by s, by
 // writing zeros (bytes containing '\0') to that area.
-
-#include "libft.h"
-
 void	ft_bzero(void *s, size_t n)
 {
 	size_t			i;
