f2fefafceda408ff8c8fffab5855a91f87d7f8f0
Leonardo Marino <leo-marino@hotmail.it>
2025-01-30 18:14:03 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,10 +1,9 @@
+#include "libft.h"
+
  // The calloc() function allocates memory for an array of nmemb elements of size bytes each and returns a pointer
  //       to the allocated memory.  The memory is set to zero.  If nmemb or size is  0,  then  calloc()  returns  either
  //       NULL,  or  a  unique  pointer value that can later be successfully passed to free().  If the multiplication of
  //       nmemb and size would result in integer overflow, then calloc() returns an  error.
-
-#include "libft.h"
-
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*str;
